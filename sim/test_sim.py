220a9f7518bc9f46e57eb77c1b5347bedc254282
Ryan.K <ryankung@ieee.org>
2018-02-09 16:12:45 +0800

    added some ut

--- sim/test_sim.py
+++ sim/test_sim.py
@@ -12,14 +12,19 @@ def test_basic():
     assert C(start + 1).auction('satoshi', 50) is False
     C(start + 20).auction('satoshi', 200)
 
-    C(start + gap + 1).auction('satoshi', 1000)
+    C(start + gap).auction('satoshi', 1000)
     assert C.cycle == 1
 
     assert C.min_bid == 200
     assert C.reserve == 200
     assert C.balance('satoshi') == 1000
 
-    C(start + gap * 2 + 1).auction('satoshi', 1000)
+    C(start + gap * 2).auction('satoshi_blalba', 1000)
     assert C.cycle == 2
     assert C.balance('satoshi') == 2000
-    assert C.redeem('satoshi', 1000)
+
+    C(start + gap * 3).auction('satoshi_blalba', 1000)
+    assert C.cycle == 3
+    assert C.redeem('satoshi', 1000) > 0
+    assert C.redeem('satoshi_blalba', 1000) > 0
+
